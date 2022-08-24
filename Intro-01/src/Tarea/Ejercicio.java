ffab538e60f664a7904eb223e78ce535c93233ce
Kevin Pocon <inkev.ap@gmail.com>
2022-08-23 23:18:43 -0600

    Tarea

---
+++ Intro-01/src/Tarea/Ejercicio.java
@@ -0,0 +1,16 @@
+package Tarea;
+
+public class Ejercicio {
+    public static void main(String[] args) {
+        int edad = 18;
+        long numero = 49606065;
+        double altura = 1.68d;
+        boolean programador = true;
+        String Nombre = "Kevin Pocon";
+        System.out.println(edad);
+        System.out.println(numero);
+        System.out.println(altura);
+        System.out.println(programador);
+        System.out.println(Nombre);
+    }
+}
